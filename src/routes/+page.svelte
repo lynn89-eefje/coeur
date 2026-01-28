2031c1e4d07030300aaa0a8f14c21c9d375d0137
Lynn <lynn89sudo@gmail.com>
2026-01-28 00:10:21 +0000

    Added FAQ and sugar rush

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import { onMount } from "svelte";
+    import { base } from "$app/paths";
+    import Footer from "$lib/footer.svelte";
 
     let mobile = $state("");
     let charY = $state(0);
     onMount(() => {
-        if (window.innerWidth <= 800) {
+        if (window.innerWidth <= 900) {
             mobile = "Mobile";
         }
         else {
@@ -65,8 +67,13 @@
         z-index: 0;
     }
 
-    #wave {
-        animation: wave 5s infinite ease-in-out;
+    .wave {
+        animation: wave 4s infinite ease-in-out;
+    }
+
+    .waveDown {
+        animation: waveDown 5s infinite ease-in-out;
+        animation-delay: 2s;
     }
 
     @keyframes wave {
@@ -74,7 +81,19 @@
             transform: translateY(0);
         }
         50% {
-            transform: translateY(12px);
+            transform: translateY(8px);
+        }
+        100% {
+            transform: translateY(0px);
+        }
+    }
+
+    @keyframes waveDown {
+        0% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(-8px);
         }
         100% {
             transform: translateY(0px);
@@ -143,17 +162,73 @@
         }
     }
 
-    #submit {
+    #sugarRush {
         background-color: rgb(129, 19, 65);
         text-align: center;
         position: relative;
+        padding-bottom: 20px;
+
+        h1, h2, p {
+            padding: 20px;
+        }
+
+        #sugarGrid {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+            grid-gap: 20px;
+            margin-left: 20px;
+            margin-right: 20px;
+
+            div {
+                background-color: rgb(161, 86, 118);
+                border: 5px solid rgb(182, 110, 140);
+                border-radius: 20px;
+                padding-top: 20px;
+                h3, p {
+                    padding: 10px;
+                }
+                h3 {
+                    font-family: Montserrat, Gabarito, Futura;
+                    font-weight: 800;
+                }
+            }
+        }
+    }
+
+    #submit {
+        background-color: rgb(162, 72, 80);
+        text-align: center;
+    }
+
+    #faq {
+        background-image: url("/images/landing/1.png");
+        background-color: rgb(130, 30, 65);
+        padding-bottom: 20px;
+        text-align: center;
+        h1 {
+            padding: 20px;
+        }
+        div {
+            h2, p {
+                padding: 10px;
+            }
+            background-color: rgb(161, 86, 118);
+            border: 5px solid rgb(182, 110, 140);
+            border-radius: 20px;
+            padding-top: 20px;
+            margin: 20px;
+        }
     }
 </style>
 <div id="landing">
+    {#if mobile == ""}
+    <img src="/images/landing{mobile}/1.png" style:transform="translateY({charY*0.5*mobileFactor}px)" alt="background"/>
+    {:else}
     <img src="/images/landing{mobile}/1.png" alt="background"/>
+    {/if}
     <img src="/images/landing{mobile}/2.png" style:transform="translateY({charY*0.5*mobileFactor}px)" id="titleLogo" translate="no" alt="Title" class="graphic"/>
     <img src="/images/landing{mobile}/3.png" class="graphic" alt="Orpheus and Heidi" />
-    <img src="/images/landing{mobile}/4.png" class="graphic" alt="Pink Wave" id="wave"/>
+    <img src="/images/landing{mobile}/4.png" class="graphic wave" alt="Pink Wave"/>
     <img src="/images/landing{mobile}/5.png" class="graphic" alt="Orpheus's Hands" />
 </div>
 <div id="ys">
@@ -168,12 +243,12 @@
     </div>
 </div> 
 <div id="ws">
-    <img src="/images/wave1.png" alt="Wave graphic" style:width="100%" style:user-select="none" style:-webkit-user-drag="none"/>
+    <img class="waveDown" src="/images/wave1.png" alt="Wave graphic" style:width="100%" style:user-select="none" style:-webkit-user-drag="none"/>
     <div id="grid" class:mobile={mobile != ""}>
         {#if mobile == ""}
         <div>
             <img translate="no" src="/images/sweatshirt.png" alt="Sweatshirt" style:max-width=60% style:height="auto" style:transform="rotate(-2deg)" style:border-radius=30px/>
-            <p>Each item requires hearts to purchase. You can earn hearts from your submissions; the amount of hearts for each submission depends on the quality of your project and the duration of time you put into making it.</p>
+            <p>Each item requires hearts to purchase. You can earn hearts from your submissions; the amount of hearts for each submission depends on the quality of your project, duration of time you put into making it, and sugar rushes <i>(next section)</i>.</p>
         </div>
         <div>
             <h1 style:font-size="50px">Earn Prizes</h1>
@@ -186,15 +261,45 @@
             <h3>The shop includes heart themed merch and even a Hack Club sweatshirt!</h3>
             <button style:margin-top="20px" style:width="60%">Check out the Shop</button>
         </div>
-        <div>
+        <div> 
             <img style:border-radius=30px style="display: flex; align-self: center;" translate="no" src="/images/sweatshirt.png" alt="Sweatshirt" style:max-width=60% style:height="auto" style:transform="rotate(-2deg)"/>
-            <p>Each item requires hearts to purchase. You can earn hearts from your submissions; the amount of hearts for each submission depends on the quality of your project and the duration of time you put into making it.</p>
+            <p>Each item requires hearts to purchase. You can earn hearts from your submissions; the amount of hearts for each submission depends on the quality of your project, duration of time you put into making it, and sugar rushes <i>(next section)</i>.</p>
         </div>
-        {/if}
+        {/if} 
     </div>
 </div>
+<div id="sugarRush">
+    <img class="waveDown" src="/images/wave2.png" alt="Wave graphic" style:width="100%" style:user-select="none" style:-webkit-user-drag="none"/>
+    <h1 style:font-size=50px>Sugar Rushes!</h1>
+    <p style:margin-bottom=30px>You can earn more hearts for each project you submit by getting sugar rushes. Sugar rushes are bonuses for completing tasks and pushing your bounds!</p>
+    {#if mobile == ""}
+    <img src="/images/donut.png" alt="Orpheus in a giant donut" style="width: 30%; height: auto"/>
+    {:else}
+    <img src="/images/donut.png" alt="Orpheus in a giant donut" style="width: 50%; height: auto;"/>
+    {/if}
+    <h2 style:font-family="Playwrite DK Loopet, Montserrat, Gabarito" style:margin-bottom=20px>Sugar Rush Bonuses</h2>
+    <div id="sugarGrid">
+        <div>
+            <h3>+2 Hearts</h3>
+            <p>Full usability through keyboard. Users should be able to fully control what pages they are on without having to use buttons or elements on the UI.</p>
+        </div>
+        <div>
+            <h3>+3 Hearts</h3>
+            <p>Incorporate music, speech, or both to your project.</p>
+        </div>
+        <div>
+            <h3>+5 Hearts</h3>
+            <p>Exceptional use of animations and graphics. All illustrations should also be original.</p>
+        </div>
+        <div>
+            <h3>+5 Hearts</h3>
+            <p>Use a web-framework you've never learned or used before!</p>
+        </div>
+    </div>
+    <p>More may be added in the future. Check back here periodically!</p>
+</div>
 <div id="submit">
-    <img src="/images/wave2.png" alt="Wave graphic" style:width="100%" style:user-select="none" style:-webkit-user-drag="none"/>
+    <img src="/images/wave3.png" alt="Wave graphic" class="waveDown" style="width: 100%; user-select: none; -webkit-user-drag: none" />
 
     <h1 style="padding: 20px;">Ready to submit?</h1>
     <p style="margin-bottom: 30px; padding: 20px;">That's great! Our submissions form isn't up yet; however, when it is, you can check back here!</p>
@@ -204,3 +309,28 @@
     <img src="/images/envelope.png" alt="Envelope" style="max-width: 200px; transform: rotate(3deg);"/>
     {/if}
 </div>
+<div id="faq">
+    <img class="waveDown" alt="Wave graphic" src="/images/wave4.png" style="width: 100%; height: auto; user-select: none; -webkit-user-drag: none" /> 
+    <h1 style:text-shadow="0px 0px 20px grey">FAQ</h1>
+    <div>
+        <h2>Can I participate in this program?</h2>
+        <p>If you are 13-18 and in high school, then yes! However, we aren't able to ship items or distribute grants in some countries <i>(most countries are supported)</i>. Let us know if you have any doubts about your ability to recieve prizes!</p>
+    </div>
+    <div>
+        <h2>Can I submit multiple projects?</h2>
+        <p>Yes! You can make multiple e-cards; however, you should challenge yourself to add more features to each new submission <i>(we will check the progression of your submissions)</i>. For prizes such as the sweatshirt, you won't likely get enough hearts from one submission to purchase.</p>
+    </div>
+    <div>
+        <h2>How many hours do I need to put into a project at minimum?</h2>
+        <p>You need to put in 6 hours for each submission, at a minimum.</p>
+    </div>
+    <div>
+        <h2>If I get a grant for an item, how can I use it?</h2>
+        <p>All grants will be processed and distributed on <a href="https://hcb.hackclub.com">HCB</a>. You should signup on the platform and use the same email that you use on your submission form.</p>
+    </div>
+    <div>
+        <h2>I have more questions!</h2>
+        <p>Ask your questions in the <i>{"#coeur"}</i> channel on the Hack Club slack!</p>
+    </div>
+</div>
+<Footer />
